8b377e0ee92011f7a7d85d50413e7fd6993cfa3c
Udoozy <agbo4samson@gmail.com>
2024-07-29 21:38:42 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,25 +3,16 @@
 Python scrpting that uses the REST API for EMP_ID
 """
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        user = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-        content = requests.get("{}user/{}".format(url, user))
-        name = content.json().get("name")
-        if name is not None:
-            jcontent = requests.get(
-                    "{}todos?userId={}".format(
-                        url, user)).json()
-            all_content = len(jcontent)
-            comp_content = []
-            for i in jcontent:
-                if i.get("completed") is True:
-                    comp_content.append(i)
-            count = len(comp_content)
-            print("Employee {} is done with tasks({}/{}):"
-                    .format(name, count, all_content))
-            for Title in comp_content:
-                print("\t {}".format(Title.get("Tittle")))
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "user/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", contents={
+        "userId": sys.argv[1]}).json()
+
+    comp_contents = [t.get("title") for t in todos if t.get(
+        "completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(comp_contents), len(todos)))
+    [print("\t {}".format(i)) for i in comp_contents]
