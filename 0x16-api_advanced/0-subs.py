9c24f0936e66810957843f0048d8e10e0df27f8d
Udoozy <agbo4samson@gmail.com>
2024-08-13 15:17:16 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,12 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """this return the total numbers of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-      "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    url = requests.get(
+      "https://www.reddit.com/r/{}/about.json".format(subreddit),
+      headers={"User-Agent": "Custom"},
+    )
+
+    if url.status_code == 200:
+        return url.json().get("data").get("subscribers")
+    else:
         return 0
-    data = response.json().get("data")
-    return data.get("subscribers")
