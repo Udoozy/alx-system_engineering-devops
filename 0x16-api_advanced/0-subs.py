184165a8143dad3577704d0657eafa28932ea760
Udoozy <agbo4samson@gmail.com>
2024-08-13 14:43:35 +0100

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+function that queries the Reddit API and return no of subscriber
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """this return the total numbers of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+      "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    data = response.json().get("data")
+    return data.get("subscribers")
