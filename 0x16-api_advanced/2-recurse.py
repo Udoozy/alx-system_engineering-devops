d23dcb4d42259c8f8988e76353dbfd6354bcd222
Udoozy <agbo4samson@gmail.com>
2024-08-13 22:01:21 +0100

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,27 +5,25 @@ Recursive Function that Queries Reddit API
 import requests
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
+def recurse(subreddit, hot_list=[], after=None):
     """This Function is to query Reddit API"""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-                "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-        }
-    params = {
-                "after": after,
-                "count": count,
-                "limit": 100
-        }
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {'User-Agent': 'my-reddit_app'}
+    params = {'after': after, 'limit': 100}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code == 404:
+
+    if response.status_code != 200:
         return None
 
-    datas = response.json().get("data")
-    after = datas.get("after")
-    count += datas.get("dist")
-    for c in datas.get("children"):
-        hot_list.append(c.get("data").get("title"))
-    if after is not None:
-        return recurse(subreddit, hot_list, after, count)
-    return hot_list
+    datas = response.json().get('data', {})
+    children = datas.get('children', [])
+
+    if not children:
+        return hot_list if hot_list else None
+
+    hot_list.extend([child.get('data', {}).get('title') for child in children])
+    if after is None:
+        return hot_list
+
+    return recurse(subreddit, hot_list, after)
