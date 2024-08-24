d1792c912d97dc4a778617728ee0255851fd7f90
Udoozy <agbo4samson@gmail.com>
2024-08-24 16:22:19 +0100

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,6 @@
 # This resolve the ammount of user active
 
-exec {'replace-1':
-  source  => shell,
-  command => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  before  => Exec['replace-2'],
-}
-
-exec {'replace-2':
-  source  => shell,
-  command => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+exec {'update_config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin:/bin:/usr/sbin',
 }
